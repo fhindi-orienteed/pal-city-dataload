4c718dd62c424834c69b3c67101f8216885b2db9
Masa-Juide2025 <s12112510@stu.najah.edu>
2026-02-25 09:32:45 +0200

    Fix folders

--- src/server/routes/taskRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const TaskController = require('../Controllers/TaskController');
-router.post('/start', TaskController.startTask);
-
-module.exports = router;
